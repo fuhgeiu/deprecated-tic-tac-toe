bbf48ab2accd38326c160ab23f2cbfcc572bf80f
alexcarnes <alexcarnes@outlook.com>
2024-06-09 18:00:01 -0500

    update print board function

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -7,7 +7,6 @@ alex carnes 6/6
 
 using namespace std;
 
-//
 
 void get_UC (char &a, char &b) {
 
@@ -24,9 +23,15 @@ void print_CB (char array_B[], string lines) {
     cout << " " << array_B[3] << " | " << array_B[4] << " | " << array_B[5] << endl;
     cout << lines << endl;
     cout << " " << array_B[6] << " | " << array_B[7] << " | " << array_B[8] << endl;
-
 }
 
+char symbol_C (int &i, char a, char b) {
+
+    char current_C;
+    i++;
+    if (i%2 == 1) return a;
+    else   { if (i%2 == 0) return b;}
+}
 
 
 
@@ -38,14 +43,22 @@ int main () {
 //    get user input on selection
 //    keep count to use differnt symbols
 //    print updated boards
-
+    bool c = 1;
+    int i =0;
     char a,b;
     char array_B [9] = {'1','2','3','4','5','6','7','8','9'};
     string lines = "---+---+---";
-
+//gets player symbols
     get_UC (a,b);
 //    cout << a << b;
-    print_CB (array_B,lines);
+//Ddetermines character to use when printing for turns
+//temporary varibale
+   int T;
+//temporary code to test function
+   while (c) { cin >> T; cout << symbol_C (i,a,b);}
+
+
+
 
 
 
